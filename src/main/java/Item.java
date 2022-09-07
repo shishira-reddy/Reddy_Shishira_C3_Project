77af3473e5bb16881de3871ca7fe53b5f103e5fe
shishira-reddy <mekalashishira1999@gmail.com>
2022-09-07 14:40:18 +0530

    Part 3: Solution

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -2,15 +2,28 @@ public class Item {
     private String name;
     private int price;
 
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
         public Item(String name, int price) {
             this.name = name;
             this.price = price;
         }
 
+    public String getName() {
        public String getName() {
         return name;
     }
+    @Override
+    public String toString(){
+        return  name + ":"
+                + price
+                + "\n"
+                ;
+    }
 
+    //public int getPrice(){return price;}
       @Override
        public String toString(){
            return  name + ":"
